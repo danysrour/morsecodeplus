d0f2d250a57eeab6c0772e4e68c353533c66df12
danysrour <dany.srour@gmail.com>
2023-06-10 13:02:42 +0300

    code morsecode with more functionality

---
+++ morsecodeplus/__init__.py
@@ -0,0 +1,28 @@
+"""
+morsecodeplus Package
+"""
+
+# Import necessary modules or define package-level imports
+
+# You can include any initialization code here if needed
+
+# Optionally, you can define a package-level __all__ variable
+# to specify the public interface of your package
+
+# __all__ = []
+
+# You can also define any package-level variables or constants
+
+# For example:
+# VERSION = "1.0.0"
+
+# Finally, you can include any package-level functions or classes
+
+# For example:
+# def encode(text):
+#     # Encode the given text into Morse code
+#     pass
+
+# def decode(morse_code):
+#     # Decode the given Morse code into text
+#     pass
